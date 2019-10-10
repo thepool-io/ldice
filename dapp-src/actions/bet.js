1ec5806edfeb2b06c2ef184957b7501349c8aec2
Corbifex <sander@moosty.com>
2019-10-10 20:55:09 +0200

    Adding roll module with deterministic random number generator

--- dapp-src/actions/bet.js
+++ dapp-src/actions/bet.js
@@ -4,8 +4,6 @@ const {
     BigNum,
 } = require('lisk-sdk');
 
-const random = require('random');
-
 class BetTransaction extends BaseTransaction {
     static get TYPE () {
         return 12;
@@ -37,50 +35,13 @@ class BetTransaction extends BaseTransaction {
     applyAsset(store) {
         const errors = [];
 
-        //rolling the number
-        const rolled_number = random.int(1,100);
-
         //read account and transaction details
         var sender = store.account.get(this.senderId);
         const CurrentBalance = BigNum(sender.balance);
-        const BetNumber = parseInt(this.asset.bet_number-1);
         const BetAmount = BigNum(this.amount);
 
-        //define profit variables
-        var PureProfit = BigNum(0);
-        var TotalProfit = BigNum(0);
-
-        //check if bet won or lost
-        if (rolled_number <= BetNumber) {
-            //calculate pure profit
-            PureProfit = BetAmount.mul(BigNum(100-BetNumber)).div(BetNumber).add(BetAmount).mul(990).div(1000).sub(BetAmount);
-
-            //remove floating point, probably should be done differently, but BigNum lacks of this feature?
-            if (PureProfit.toString().includes(".")) {
-                PureProfit = BigNum(PureProfit.toString().split(".")[0]);
-            }
-
-            //calculate total profit
-            TotalProfit = BetAmount.add(PureProfit);
-
-            //save to sender account
-            sender.balance = CurrentBalance.add(PureProfit).toString();
-        } else {
-            //deduce cost of lost bet
-            TotalProfit = BetAmount.neg(BetAmount);
-
-            //save to sender account
-            sender.balance = CurrentBalance.sub(BetAmount).toString();
-        }
-
-        //prepare result bet to store in sender account asset
-        const this_tx = {[this.id]:{bet_number: this.asset.bet_number, profit: TotalProfit.toString(), rolled_number: rolled_number}};
-        if (sender.asset.transaction_results === undefined) {
-            sender.asset.transaction_results = [];
-        }
-
-        //append array with bet result tx
-        sender.asset.transaction_results.push(this_tx);
+        // update balance
+        sender.balance = CurrentBalance.sub(BetAmount).toString();
 
         //save
         store.account.set(sender.address, sender);
---
+++ dapp-src/modules/roll/roll.js
@@ -0,0 +1,110 @@
+/*
+ * Copyright © 2019 Lisk Foundation
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with the Lisk Foundation,
+ * no part of this software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ */
+
+'use strict';
+
+const {createLoggerComponent} = require('lisk-framework/src/components/logger');
+const {createStorageComponent} = require('lisk-framework/src/components/storage');
+const {
+  bootstrapStorage,
+} = require('./init_steps');
+const rollDice = require('./actions/roll_dice');
+
+module.exports = class Roll {
+  constructor(channel, options) {
+    this.channel = channel;
+    this.options = { blockHashDistance: 1, ...options };
+    this.logger = null;
+    this.scope = null;
+    if (this.options.blockHashDistance < 1 || this.options.blockHashDistance > 10) {
+      this.options.blockHashDistance = 2;
+    }
+  }
+
+  async bootstrap() {
+    global.constants = this.options.constants;
+
+    // Logger
+    const loggerConfig = await this.channel.invoke(
+      'app:getComponentConfig',
+      'logger',
+    );
+    this.logger = createLoggerComponent({
+      ...loggerConfig,
+      module: 'roll',
+    });
+
+    // Storage
+    this.logger.debug('Initiating storage...');
+    const storageConfig = await this.channel.invoke(
+      'app:getComponentConfig',
+      'storage',
+    );
+    const dbLogger =
+      storageConfig.logFileName &&
+      storageConfig.logFileName === loggerConfig.logFileName
+        ? this.logger
+        : createLoggerComponent({
+          ...loggerConfig,
+          logFileName: storageConfig.logFileName,
+          module: 'roll:database',
+        });
+    const storage = createStorageComponent(storageConfig, dbLogger);
+
+    const applicationState = await this.channel.invoke(
+      'app:getApplicationState',
+    );
+
+    // Setup scope
+    this.scope = {
+      components: {
+        logger: this.logger,
+        storage,
+      },
+      channel: this.channel,
+      config: this.options,
+      applicationState,
+    };
+    await bootstrapStorage(this.scope, global.constants.ACTIVE_DELEGATES);
+
+    rollDice(this.scope, this.logger);
+  }
+
+  async cleanup(code, error) {
+    const {components} = this.scope;
+    if (error) {
+      this.logger.fatal(error.toString());
+      if (code === undefined) {
+        code = 1;
+      }
+    } else if (code === undefined || code === null) {
+      code = 0;
+    }
+    this.logger.info('Cleaning Roll module...');
+
+    try {
+      if (components !== undefined) {
+        Object.keys(components).forEach(async key => {
+          if (components[key].cleanup) {
+            await components[key].cleanup();
+          }
+        });
+      }
+    } catch (componentCleanupError) {
+      this.logger.error(componentCleanupError);
+    }
+    this.logger.info('Cleaned up successfully');
+  }
+
+};
