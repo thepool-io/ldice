9dedb953390dc24e75a2892dd563bc9218bf4680
karek314 <karek314@protonmail.ch>
2020-02-02 23:53:10 +0800

    decouple front-end

--- app-html/dapp_js/dapp_math.js
+++
@@ -1,3 +0,0 @@
-function CalculateBetProfit(BetAmount,NumberOfBet){
-	return ((((BetAmount * (100-parseInt(NumberOfBet))) / parseInt(NumberOfBet)+BetAmount))*GLOBAL_EDGE/GLOBAL_EDGE_DIVISOR)-BetAmount;
-}
