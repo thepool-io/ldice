99d5df91b3d1e7d286d6d3bae94faf401a9a8e86
karek314 <karek314@protonmail.ch>
2020-02-15 19:44:15 +0800

    update Profit
    -removed redundant parse because of native BitInt 
    -moved configurable house edge to constructor

--- logic/profit.js
+++ logic/profit.js
@@ -4,13 +4,11 @@ module.exports = class Profit {
     constructor(betNumber, betAmount) {
       this.betNumber = BigInt(betNumber);
       this.betAmount = BigInt(betAmount);
+      this.houseEdge = 990;// = 99% (1% houseEdge)
+      this.houseEdgeDivisor = 1000;
     }
     get() {
-      let pureProfit = this.betAmount*(BigInt(100)-this.betNumber)/this.betNumber+this.betAmount*BigInt(990)/BigInt(1000)-this.betAmount;
-      //remove floating point, probably should be done differently, but BigNum/BigInt lacks of this feature? CHECK AFTER BIGNUM->BigInt
-      if (pureProfit.toString().includes(".")) {
-        pureProfit = BigInt(pureProfit.toString().split(".")[0]);
-      }
+      let pureProfit = this.betAmount*(BigInt(100)-this.betNumber)/this.betNumber+this.betAmount*BigInt(this.houseEdge)/BigInt(this.houseEdgeDivisor)-this.betAmount;
       return pureProfit;
     }
 }
