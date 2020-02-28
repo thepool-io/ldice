a11ea85b5ca8da5860696be2a12ca360aedffccc
karek314 <karek314@protonmail.ch>
2020-02-28 13:54:37 +0800

    add license & minor fix

--- modules/websocket/wsRequests.js
+++ modules/websocket/wsRequests.js
@@ -1,4 +1,29 @@
+/*
+MIT License
+
+Copyright (c) 2020 ThePool.io
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
 'use strict';
+
 const Draw = require('../../logic/draw.js');
 
 module.exports = ({components}, socket) => {
@@ -36,7 +61,7 @@ module.exports = ({components}, socket) => {
                                             lastbets[i].signature,
                                             lastbets[i].asset.data,
                                             lastbets[i].asset.amount).get();
-                tidyLastBets = [...tidyLastBets, {id: lastbets[i].id, sender: lastbets[i].senderId, profit: drawResult.totalProfit.toString(), bet_number: drawResult.betNumber.toString(), rolled_number: drawResult.rolledNumber.toString()}]
+                tidyLastBets = [...tidyLastBets, {id: lastbets[i].id, sender: lastbets[i].senderId, profit: drawResult.totalProfit.toString(), bet_number: (drawResult.betNumber+1).toString(), rolled_number: drawResult.rolledNumber.toString()}]
             }
         }
         socket.emit(`lastbets`, tidyLastBets);
